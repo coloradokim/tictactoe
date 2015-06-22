8d5e1e89272d319d538dd493d01196abbe956b8b
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-06-22 17:51:51 -0600

    Xs and Ox Work

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ var xMoves =[]
 var boxes = document.getElementsByClassName('box')
 
 // //track play
-// var turn = true;
-// var playsX = [];
-// var playsO = [];
+var turn = true;
+var playsX = [];
+var playsO = [];
 
-// store winning combinations
-	// var winningCombos = [
-	// 	[1,2,3],
-	// 	[1,4,7],
-	// 	[1,5,9],
-	// 	[4,5,6],
-	// 	[3,6,9],
-	// 	[3,5,7],
-	// 	[7,8,9],
-	// 	[2,5,8]
-	// ];
+// The winning patterns
+	var winningCombos = [
+		[1,2,3],
+		[1,4,7],
+		[1,5,9],
+		[4,5,6],
+		[3,6,9],
+		[3,5,7],
+		[7,8,9],
+		[2,5,8]
+	];
 
 //eventListener that is multiuse
-	function addXandOListener(){
+	function XOListener(){
   for (var i = boxes.length - 1; i >= 0; i--) {
     boxes[i].addEventListener("click", addXorO);
   }
 }
-addXandOListener();
+XOListener();
 
 //XorO making
   function addXorO(event){
   if (event.target.innerHTML.length === 0){ //if box is empty
-    if (counter % 2 === 0) {
+    if (counter % 2 === 0) { //if the counter is an odd number
       oMoves.push(parseInt(event.target.getAttribute("data-num")));
       event.target.innerHTML = "O";
       counter++;
@@ -46,17 +46,34 @@ addXandOListener();
 	}
 	console.log('the counter is ' + counter);
 	if (counter === 9){
-		alert("Game Over!");
+		alert("The game is a draw. Play again!");
 	}
 }
 
-// Clear all added styles, content and reset the turn
-	// var clearGame = function() {
-	// 	for (var i = 0; i < boxes.length; i++) {
-	// 		boxes[i].innerHTML = '';
-	// 		boxes[i].className = 'box';
-	// 	}
-	// 	playsX = [];
-	// 	playsO = [];
-	// 	turn = true;
-	// };
+
+//Not working
+function checkForWin(movesArray, name){
+  // loop over the first array of winning combinations
+  for (i = 0; i < winningCombinations.length; i++) {
+    // reset the winCounter each time!
+    winCounter = 0;
+    // loop over each individual array
+    for (var j = 0; j < winningCombinations[i].length; j++) {
+      // if the number in winning combo array is === a number in moves array, add to winCounter
+      if(movesArray.indexOf(winningCombinations[i][j]) !== -1){
+        winCounter++;
+      }
+      // if winCounter === 3 that means all 3 moves are winning combos and game is over!
+      if(winCounter === 3){
+        alert("Game over, " + name + " wins!");
+        resetBoard();
+      }
+    }
+  }
+}
+
+//click reset button to reload the page (not working)
+function resetBoard(){
+	var resetButton = document.getElementById('reset')
+  resetButton.addEventListener('click', location.reload());
+}
